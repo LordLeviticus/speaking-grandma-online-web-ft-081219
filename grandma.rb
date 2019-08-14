2290f9d9f982ec7ee73cb4b7ca584e37e750dd94
Levi Borland <borland.levi@gmail.com>
2019-08-14 08:13:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
-  if !phrase == phrase.upcase
-    return "HUH? SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-    return "NO, NOT SICE 1938!"
+  if phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LIVE YOU TOO PUMPKIN!"
-  end
+    return "I LIVE YOU TO PUMPKIN!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+end
 
 # Write a speak_to_grandma method.
 
