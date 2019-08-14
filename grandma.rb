dcf152091cc0859072c48b36717cdf85199db92a
Levi Borland <borland.levi@gmail.com>
2019-08-14 08:14:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LIVE YOU TO PUMPKIN!"
+    return "I LIVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
